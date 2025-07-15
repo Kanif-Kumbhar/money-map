652cc4259c4c2db4422b55b4c7e8cb3af9f2900b
Kanif <kumbharkanif666@gmail.com>
2025-07-15 16:38:02 +0530

    feat: complete transaction creation flow and fix minor UI bugs

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ function layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="relative flex h-screen w-full flex-col items-center justify-center">
 			<Logo />
-			<div className="mt-12">{children}</div>
+			<div className="">{children}</div>
 		</div>
 	);
 }
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,16 @@
-import { PiggyBank } from 'lucide-react';
-import Link from 'next/link';
-import React from 'react'
+import { Dog } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 function Logo() {
-  return <Link href="/" className='flex items-center gap-2'>
-    <PiggyBank className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]'/>
-    <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>MoneyMap</p>
-  </Link>;
+	return (
+		<Link href="/" className="flex items-center gap-2">
+			<Dog className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
+			<p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+				MoneyMap
+			</p>
+		</Link>
+	);
 }
 
 export function LogoMobile() {
@@ -19,4 +23,4 @@ export function LogoMobile() {
 	);
 }
 
-export default Logo
+export default Logo;
